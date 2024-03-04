0ed8f8e108d8ce7052f60eba0f89a330e180dd75
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-04 00:27:21 -0600

    Practica 21

---
+++ 021_Cond_IF_ELSE.py
@@ -0,0 +1,48 @@
+#Practica 21    	El condicional IF ELSE.									
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+#Declaramos nuestras variables con sus respectivos valores
+a = 1.6
+b = 1.6
+
+#Realizamos nuestras comparaciones 
+if a == b:
+    print('El valor del dato a y b son iguales')
+
+else:
+    print('El valor del dato a y b no son iguales') 
+
+#Realizamos nuestras comparaciones 
+if a != b:
+    print('El valor del dato a y b son diferentes')
+
+else:
+    print('El valor del dato a y b no son diferentes')
+    
+#Realizamos nuestras comparaciones 
+if a > b:
+    print('El valor del dato a es mayor que el dato b')
+  
+else: 
+    print('El valor del dato a no es mayor que el dato b')
+  
+#Realizamos nuestras comparaciones 
+if a < b:
+    print('El valor del dato b es mayor que el dato a')
+
+else: 
+    print('El valor del dato b no es mayor que el dato a')
+
+#Realizamos nuestras comparaciones     
+if a >= b:
+    print('El valor del dato a es mayor o igual que el dato b')
+  
+else: 
+    print('El valor del dato a no es mayor o igual que el dato b')
+
+#Realizamos nuestras comparaciones     
+if a <= b:
+    print('El valor del dato b es mayor o igual que el dato a')
+
+else: 
+    print('El valor del dato a no es mayor o igual que el dato b')

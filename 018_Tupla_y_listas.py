560236c38e27d071e14451eef8419efb956669f2
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-03 23:43:19 -0600

    Practica 18

---
+++ 018_Tupla_y_listas.py
@@ -0,0 +1,17 @@
+# Codigo para utilizar acentos 
+#----------------------------------↓
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_ 
+#----------------------------------↑
+#Practica 18    Crear y manejar tuplas y diferencias entre listas.							
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+#Declaramos nuestra variable-lista con '[]'
+Com =   ['CPU', 'MB', 'PSU', 'RAM', 'SSD', 'FAN' , 'CASE' , 'HDD']
+
+#Declaramos nuestra variable-tupla con '()'
+Precio =   ['3000', '2000', '3500', '1000', '1500', '500' , '2500' , '500', 'El precio del componente ']
+
+print(Precio[-1] , Com[-1] , Precio[-2])

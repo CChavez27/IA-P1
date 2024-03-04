664aab9e3746d15c3e17914772248877898eca92
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-03 22:59:39 -0600

    Practica 07

---
+++ 007_Exponentes.py
@@ -0,0 +1,18 @@
+# Codigo para utilizar acentos 
+#----------------------------------↓
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_ 
+#----------------------------------↑
+#Practica 7    	Cómo calcular exponentes.
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+#Declaramos el valor de nuestras variables
+a =   10
+b =   2
+
+#Realizamos la operacion de potencia usando 'a**b'
+potencia  = a**b
+
+print('El resultado de ', a ,' elevado a ', b ,'\n\t', potencia)

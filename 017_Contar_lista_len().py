e297b92ef62b43ca210779d9b0e932f74570963b
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-03 23:22:51 -0600

    Practica 17

---
+++ 017_Contar_lista_len().py
@@ -0,0 +1,18 @@
+
+# Codigo para utilizar acentos 
+#----------------------------------↓
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_ 
+#----------------------------------↑
+#Practica 17    	Contar elementos con len().						
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+
+#Declaramos nuestra variable-lista con '[]'
+Com =   ['CPU', 'MB', 'PSU', 'RAM', 'SSD', 'FAN' , 'CASE' , 'HDD']
+print('Lista de componentes para una PC GAMER\n\t', Com)
+
+#Se ejecuta el comando '.sort()' para ordenar la  lista de manera alfabetica 
+print('\nCantidad de componentes total\n\t',len(Com))

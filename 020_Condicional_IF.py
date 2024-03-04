2255740a345f4ad608d3de1b98b4c2d0521fc9ea
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-04 00:17:16 -0600

    Practica 20

---
+++ 020_Condicional_IF.py
@@ -0,0 +1,25 @@
+#Practica 20    	El condicional IF.								
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+#Declaramos nuestras variables con sus respectivos valores
+a = 27
+b = 9
+
+#Realizamos nuestras comparaciones
+if a == b:
+    print('El valor del dato a y b son iguales')
+
+if a != b:
+    print('El valor del dato a y b son diferentes')
+
+if a > b:
+    print('El valor del dato a es mayor que el dato b')
+  
+if a < b:
+    print('El valor del dato b es mayor que el dato a')
+
+if a >= b:
+    print('El valor del dato a es mayor o igual que el dato b')
+  
+if a <= b:
+    print('El valor del dato b es mayor o igual que el dato a')

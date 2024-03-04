da4db52ff64ab59cdc7ee6a3ed680c7c67623eb4
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-03 23:03:16 -0600

    Practica 10

---
+++ 010_Uso_de_listas_neg.py
@@ -0,0 +1,15 @@
+# Codigo para utilizar acentos 
+#----------------------------------↓
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_ 
+#----------------------------------↑
+#Practica 10    	Posiciones negativas.	
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+#Declaramos nuestra variable y su lista
+materias = ['IA', 'Microcontroladores', 'Teoria de control', 'vision artificial', 'plc']
+
+#se imprime la lista que llamemos usando el signo negativo
+print('La ultima materia del dia es:\n\t' ,materias[-1].upper())

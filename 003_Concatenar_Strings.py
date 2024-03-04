33b60e32b7cf4f3c8555013f23b5129677cf5799
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-03 22:48:03 -0600

    Practica 03

---
+++ 003_Concatenar_Strings.py
@@ -0,0 +1,24 @@
+# Codigo para utilizar acentos 
+#----------------------------------↓
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_ 
+#----------------------------------↑
+#Practica 4 concatenar strings 
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+#Declaramos nuestros strings
+string1 =   'Bienvenid@'        
+string2 =   'humano :D'
+
+#Concatenamos los string usando la operacion '+' 
+string3 =   string1 +   ' ' +   string2
+print(string3)
+
+#Concatenamos los string y reciclando una variable al realizar la operacion '+'
+string1 =   string1 +   ' ' +   string2
+print(string1)
+
+#concatenamos los string usando 'print'
+print(string1   +   '\n'    + 'Cómo se encuentra?')

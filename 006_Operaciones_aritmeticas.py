c621eccc3948b050cf389177d0f52bbd10599ad4
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-03 22:56:07 -0600

    Practica 06

---
+++ 006_Operaciones_aritmeticas.py
@@ -0,0 +1,23 @@
+# Codigo para utilizar acentos 
+#----------------------------------↓
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_ 
+#----------------------------------↑
+#Practica 6    Suma, resta, multiplicación y división.	
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+#Operando Suma '+'
+suma    =   27+9
+
+#Operando Resta '-'
+resta   =   suma -2
+
+#Operando Multiplicacion '*'
+mul     =   resta*2
+
+#Operando Division '/'
+div= mul/4
+
+print('Resultados\n\t',suma,'\n\t',resta,'\n\t',mul,'\n\t',div)

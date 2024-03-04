310f382b9f88d0c9aee1935c2ef1c5c65c2fdb0c
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-03 22:55:09 -0600

    Practica 05

---
+++ 005_Salto_de_linea_y_tab.py
@@ -0,0 +1,12 @@
+# Codigo para utilizar acentos 
+#----------------------------------↓
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_ 
+#----------------------------------↑
+#Practica 5     Saltos de línea y tabulaciones.
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+#Aprendiendo a usar '\n' para saltos de linea y '\t' para tabulaciones 
+print('Lenguajes de programacion\n\tPython\n\tJavaScript\n\tJava\n\tC\n\tC++\n\tPHP\n\tetc... ')

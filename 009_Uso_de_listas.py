d5f0ebe45d57656cd834e580e2fb9351fb2bb626
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-03 23:01:46 -0600

    Practica 09

---
+++ 009_Uso_de_listas.py
@@ -0,0 +1,16 @@
+# Codigo para utilizar acentos 
+#----------------------------------↓
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_ 
+#----------------------------------↑
+#Practica 9    	Cómo crear listas y utilizarlas.
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+#Declaramos nuestra variable-lista con '[]'
+Com =   ['CPU', 'MB', 'PSU', 'RAM', 'SSD', 'FAN']
+
+#Se imprime todo la lista con la variable Com 
+#Tambien se elige cual string se desea imprimir siguiendo la instruccion 'Com[0-5]' 
+print('Lista de componentes para una PC GAMER\n\t', Com , '\n\t' , Com[5])

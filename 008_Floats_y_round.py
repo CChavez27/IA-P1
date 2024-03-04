83e98a007c78295b305ec7f54dd5af282138da63
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-03 23:00:37 -0600

    Practica 08

---
+++ 008_Floats_y_round.py
@@ -0,0 +1,17 @@
+# Codigo para utilizar acentos 
+#----------------------------------↓
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_ 
+#----------------------------------↑
+#Practica 8    	Los floats y el método round().
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+#Declaramos nuestras variables de dato flotante
+a  =    27.2727
+b  =    20.0220
+suma=a+b
+
+#usamos el metodo 'round' para redondear a solo '2' numeroos decimales
+print('El resultado de ', a , '+' , b , '\n\n\t\t :',round(suma,2))

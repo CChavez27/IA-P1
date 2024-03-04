bf4d94ec565a8e92c1bce38f6e8b54f860e44459
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-03 23:25:53 -0600

    Practica 01

---
+++ 001_Usando_variables.py
@@ -0,0 +1,14 @@
+#Practica 1 Variables
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+#Declaramos nuestra variable de tipo string e imprimimos
+Mensaje = "Hola mundo" 
+print(Mensaje) 
+
+#Declaramos nuestras variables y su valor
+x = 2 
+y = 2 
+
+#Realizamos la suma de variables e imprimimos su resultado
+suma = x+y 
+print(z) 

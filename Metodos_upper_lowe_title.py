f77d24e26733d42eb1a4d1a006f082cf12cf60de
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-03-03 22:50:16 -0600

    Practica 04

---
+++ Metodos_upper_lowe_title.py
@@ -0,0 +1,23 @@
+# Codigo para utilizar acentos 
+#----------------------------------↓
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_ 
+#----------------------------------↑
+#Practica 4 Los métodos upper(), lower() y title().
+#Desarrollado por Salazar Chavez Cristian Uriel :D
+
+#Declaramos nuestras variables
+materia     =   'INTELIGENCIA ARTIFICIAL'
+instituto   =   'centro de enseñanza técnica industrial'
+nombre      =   'salazar chavez cristian uriel'
+
+#Metodo 'upper()' convierte el texto a mayusculas
+print(instituto.upper()) 
+
+#Metodo 'lower()' convierte el texto a minusculas
+print(materia.lower())
+
+#Metodo 'title()' convierte la primera letra de cada oracion en mayusculas
+print(nombre.title())
